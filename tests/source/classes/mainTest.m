4bdcdd472ad59e45cba0820421114bee8d6582b5
Gaurav Bhalerao <gvbhalerao591@gmail.com>
2024-05-02 13:48:33 +0100

    Create mainTest.m

---
+++ tests/source/classes/mainTest.m
@@ -0,0 +1,18 @@
+% create mainTest class by subclassing matlab.unittest.TestCase
+
+classdef mainTest < matlab.unittest.TestCase
+    methods(Test)
+        function func1(testCase)
+            % 
+            % your code here
+            %
+        end
+
+        function func2(testCase)
+            %
+            % your code here
+            %
+        end
+    end
+end
+            
